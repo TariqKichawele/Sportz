1c96c011c2f37b6bb325bebb92917158587c3e3a
TariqKichawele <tariqkichawele66@gmail.com>
2026-02-15 15:30:24 -0500

    implement matches POST and GET routes

---
+++ src/routes/matches.js
@@ -0,0 +1,58 @@
+import { Router } from 'express';
+import { createMatchSchema } from '../validation/matches.js';
+import { db } from '../db/db.js';
+import { matches } from '../db/schema.js';
+import { getMatchStatus } from '../utils/match.status.js';
+import { listMatchesQuerySchema } from '../validation/matches.js';
+import { desc } from 'drizzle-orm';
+
+const MAX_LIMIT = 100;
+
+export const matchesRouter = Router();
+
+matchesRouter.get('/', async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query);
+
+    if (!parsed.success) {
+        res.status(400).json({ message: parsed.error.message });
+        return;
+    }
+
+    const limit = Math.min(parsed.data.limit ?? 50, MAX_LIMIT);
+
+    try {
+        const data = await db.select().from(matches).orderBy((desc, matches.createdAt)).limit(limit);
+
+        res.status(200).json({ data });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to list matches', details: error.message });
+    }
+
+
+});
+
+matchesRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+    const { data: { startTime, endTime, homeScore, awayScore }} = parsed;
+
+    if (!parsed.success) {
+        res.status(400).json({ message: parsed.error.message });
+        return;
+    }
+
+    try {
+        const [event] = await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: 0 ?? homeScore,
+            awayScore: 0 ?? awayScore,
+            staus: getMatchStatus(startTime, endTime)
+        }).returning();
+
+        res.status(201).json({ data: event });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to create match', details: error.message });
+    }
+
+})
